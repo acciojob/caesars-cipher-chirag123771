7a1461a1a06f4f44474ff585d299337fa886c543
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2023-03-27 14:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,20 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+   // Your Result goes here
   // Only change code below this line
+	let decodedStr = "";
+  for (let i = 0; i < encodedStr.length; i++) {
+    let charCode = encodedStr.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      // Shift 13 places and handle wrapping around from Z to A
+      charCode = ((charCode - 65 + 13) % 26) + 65;
+    }
+    decodedStr += String.fromCharCode(charCode);
+  }
+  return decodedStr;
 
-  return ;//return decodedArr
+ //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
